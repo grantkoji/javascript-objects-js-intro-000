b4038f7fff651d1df9263148c2f0f9a92c6343d5
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-05 21:47:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, name, title) {
   playlist[name] = title;
   return playlist
 }
+
+function removeFromPlayList(playlist, name) {
+  delete playlist.name;
+  return playlist
+}
